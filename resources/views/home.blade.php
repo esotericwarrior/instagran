6079283267bdcf4f910a68aff2336e5300fbbae3
Tristan <tristan.santiago@ymail.com>
2020-02-13 00:08:08 -0500

    Added 'ProfileBanner' to 'home.blade.php'.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+  <div class="">
+    <profile-banner v-bind:displayname="{{ json_encode(Auth::user()->name) }}" :username="{{ json_encode(Auth::user()->email) }}" />
+    @yield('content')
+  </div>
+</div>
 @endsection
