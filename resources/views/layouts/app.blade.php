dd40e672abfb8cdc39497819ebf6c463b0fef4ab
Tristan <tristan.santiago@ymail.com>
2020-02-12 22:15:11 -0500

    Changed logo size.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div><img src="/png/logo.png" style="height: 25px; border-right: 1px solid #333;" class="pr-3"></div>
+                    <div><img src="/png/logo.png" style="height: 20px; border-right: 1px solid #333;" class="pr-3"></div>
                     <div class="pl-3">Instagran</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
