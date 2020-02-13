22b8b57e360a3d876e91c2d141d0fcea7d07136c
Tristan <tristan.santiago@ymail.com>
2020-02-12 22:12:38 -0500

    Modified logo navbar styling.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/png/logo.png" style="height: 25px"></div>
-                    <div>Instagran</div>
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/png/logo.png" style="height: 25px" class="pr-3"></div>
+                    <div class="pl-3">Instagran</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
