5858fdb7e743c77edfbbcb4e0e9287ec526d7672
Tristan <tristan.santiago@ymail.com>
2020-02-13 02:12:46 -0500

    Modified prop data being sent to 'ProfileBanner' component.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="app" class="container">
   <div class="col-12">
-    <profile-banner v-bind:displayname="{{ json_encode(Auth::user()->name) }}" :username="{{ json_encode(Auth::user()->email) }}"></profile-banner>
+    <profile-banner v-bind:displayname="{{ json_encode(Auth::user()->name) }}" :username="{{ json_encode(Auth::user()->username) }}"></profile-banner>
     <profile-pictures></profile-pictures>
     @yield('content')
   </div>
