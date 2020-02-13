fa45b0274d13bc9df8e134660b046a7b053de2a1
Tristan <tristan.santiago@ymail.com>
2020-02-12 21:42:54 -0500

    Installed Laravel Authentication package.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Instagran
                 </div>
 
                 <div class="links">
