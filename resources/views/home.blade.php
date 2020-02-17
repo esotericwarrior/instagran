5d108550c42db55c167a8b18c97880e304bfabbc
Tristan <tristan.santiago@ymail.com>
2020-02-17 15:28:25 -0500

    Added placeholder data for profile title and description.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,15 @@
 <div id="app" class="container">
   <div class="col-12">
     <profile-banner :displayname='@json($user->name)' :username='@json($user->username)'></profile-banner>
+    <div class="">
+      {{ $user->profile->title }}
+    </div>
+    <div class="">
+      {{ $user->profile->description }}
+    </div>
+    <div class="">
+      <a href="#">{{ $user->profile->url }}</a>
+    </div>
     <profile-pictures></profile-pictures>
     @yield('content')
   </div>
