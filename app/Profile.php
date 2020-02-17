9e07ec1e014ef0785add44ceca85e07f930584d2
Tristan <tristan.santiago@ymail.com>
2020-02-17 14:19:40 -0500

    Created 'user' method.

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    // Function that establishes 1-to-1 relationship of profile to user.
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
 }
