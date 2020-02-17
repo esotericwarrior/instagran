ea6cdcd98da1e4575f0b5a81ef8732c59021fd42
Tristan <tristan.santiago@ymail.com>
2020-02-17 13:31:35 -0500

    Created 'Profile' model for profile info.

---
+++ database/migrations/2020_02_17_182418_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id'); // Foreign Key: Reference to ID of user, so we know profile belongs to specific user.
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('url');->nullable();
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
