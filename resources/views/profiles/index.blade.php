b7a81d32ae55b29e1a810147f47aae7d041706dd
Tristan <tristan.santiago@ymail.com>
2020-02-18 23:04:51 -0500

    Added placeholders for user profile info and photos.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,6 +4,12 @@
 <div id="app" class="container">
   <div class="col-12">
     <profile-banner :displayname='@json($user->name)' :username='@json($user->username)'></profile-banner>
+    <div class="d-flex">
+      <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+      <div class="pr-5"><strong>{{ 1302 }}</strong> followers</div>
+      <div class="pr-5"><strong>{{ 212 }}</strong> following</div>
+    </div>
+    <a href="/p/create">Add New Post</a>
     <div class="">
       {{ $user->profile->title }}
     </div>
@@ -13,7 +19,16 @@
     <div class="">
       <a href="#">{{ $user->profile->url }}</a>
     </div>
-    <profile-pictures></profile-pictures>
+    <!-- <profile-pictures></profile-pictures> -->
+
+    <div class="row pt-5">
+      @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+          <img src="/storage/{{ $post->image }}" class="w-100">
+        </div>
+        @endforeach
+    </div>
+
     @yield('content')
   </div>
 </div>
