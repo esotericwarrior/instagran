276c71b41337fb8beae34aaab59ebd292084cfb1
Tristan <tristan.santiago@ymail.com>
2020-02-20 21:20:42 -0500

    Modified content arrangement on 'index.blade.php'.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,30 +3,25 @@
 @section('content')
 <div id="app" class="container">
   <div class="col-12">
-    <profile-banner :displayname='@json($user->name)' :username='@json($user->username)'></profile-banner>
-    <div class="d-flex">
-      <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-      <div class="pr-5"><strong>{{ 1302 }}</strong> followers</div>
-      <div class="pr-5"><strong>{{ 212 }}</strong> following</div>
-    </div>
-    <a href="/p/create">Add New Post</a>
-    <div class="">
-      {{ $user->profile->title }}
-    </div>
-    <div class="">
-      {{ $user->profile->description }}
-    </div>
-    <div class="">
-      <a href="#">{{ $user->profile->url }}</a>
-    </div>
-    <!-- <profile-pictures></profile-pictures> -->
+    <!-- If user is authenticated and is the owner of the current profile -->
+    @if (Auth::user() && Auth::user()->id == $user->id)
+      <!-- Display the owner's version of the profile header -->
+      <profile-banner :displayname='@json($user->name)' :username='@json($user->username)'></profile-banner>
+    <!-- Otherwise, display the public version of the profile header -->
+    @else
+      <public-profile-banner
+        :count='@json($user->posts->count())'
+        :description='@json($user->profile->description)'
+        :displayname='@json($user->name)'
+        :title='@json($user->title)'
+        :url='@json($user->url)'
+        :username='@json($user->username)'
+      >
+      </public-profile-banner>
+    @endif
 
     <div class="row pt-5">
-      @foreach($user->posts as $post)
-        <div class="col-4 pb-4">
-          <img src="/storage/{{ $post->image }}" class="w-100">
-        </div>
-        @endforeach
+        <profile-pictures :user='@json($user->posts)'></profile-pictures>
     </div>
 
     @yield('content')
