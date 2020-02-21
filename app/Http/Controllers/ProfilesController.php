5d321ae2ddf4ea90d20f605a059657a489f1705d
Tristan <tristan.santiago@ymail.com>
2020-02-21 00:57:49 -0500

    Function refactoring.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-  public function index(\App\User $user)
+  public function index(User $user)
   {
     return view('profiles.index', compact('user'));
   }
 
-  public function edit(\App\User $user)
+  public function edit(User $user)
   {
     return view('profiles.edit', compact('user'))
   }
