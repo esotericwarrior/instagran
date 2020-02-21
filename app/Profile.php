2dadc0e1cee990b76a209b1581614a9487e53677
Tristan <tristan.santiago@ymail.com>
2020-02-21 01:29:36 -0500

    Disabled mass assignment rules of 'Profile' model.

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    protected $guarded = []; // Disable the mass assignment rule since we're already validating in the Profiles Controller.
+
     // Function that establishes 1-to-1 relationship of profile to user.
     public function user()
     {
