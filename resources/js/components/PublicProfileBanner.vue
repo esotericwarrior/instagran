848a276b79ef9c44227e8d7bfd051e868f781559
Tristan <tristan.santiago@ymail.com>
2020-02-20 21:16:47 -0500

    Initialized 'PublicProfileBanner' component props.

--- resources/js/components/PublicProfileBanner.vue
+++ resources/js/components/PublicProfileBanner.vue
@@ -3,7 +3,32 @@
 <script>
 export default {
   name: "PublicProfileBanner",
-  props: {},
+  props: {
+    count: {
+      default: null,
+      type: Number
+    },
+    description: {
+      default: null,
+      type: String
+    },
+    displayname: {
+      type: String,
+      required: true
+    },
+    title: {
+      default: null,
+      type: String
+    },
+    url: {
+      default: null,
+      type: String
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
       isFollowed: true
