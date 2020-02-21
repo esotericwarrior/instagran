54d54274ae65bd6a8680824c7215da45ca32667d
Tristan <tristan.santiago@ymail.com>
2020-02-20 23:10:42 -0500

    Passed new props to 'ProfileBanner' component from 'index.blade.php'.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -6,7 +6,15 @@
     <!-- If user is authenticated and is the owner of the current profile -->
     @if (Auth::user() && Auth::user()->id == $user->id)
       <!-- Display the owner's version of the profile header -->
-      <profile-banner :displayname='@json($user->name)' :username='@json($user->username)'></profile-banner>
+      <profile-banner
+        :count='@json($user->posts->count())'
+        :description='@json($user->profile->description)'
+        :displayname='@json($user->name)'
+        :title='@json($user->title)'
+        :url='@json($user->url)'
+        :username='@json($user->username)'
+      >
+      </profile-banner>
     <!-- Otherwise, display the public version of the profile header -->
     @else
       <public-profile-banner
