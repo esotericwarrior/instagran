c8ada529e67155c9c4197f6f90cd60ece9f73d0d
Tristan <tristan.santiago@ymail.com>
2020-02-20 21:15:36 -0500

    Created 'PublicProfileBanner' component.

---
+++ resources/js/components/PublicProfileBanner.vue
@@ -0,0 +1,12 @@
+<template> </template>
+
+<script>
+export default {
+  name: "PublicProfileBanner",
+  props: {},
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped></style>
